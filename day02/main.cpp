d588f1f926007cb431acaba3fe480ee81e414ce3
Petr Faltýnek <pfaltynek@gmail.com>
2017-03-16 14:43:41 +0100

    day 2 solution modified to use c++ IO library and regex

--- day02/main.cpp
+++ day02/main.cpp
@@ -1,46 +1,42 @@
 #include <cstdio>
+#include <fstream>
 #include <iostream>
-#include <stdlib.h>
-#include <string.h>
+#include <regex>
+#include <string>
 
 int main(void) {
-	FILE* f;
-	char buff [101], *ptr, *pnum;
+	std::ifstream input;
 	int result1 = 0, result2 = 0;
-	bool basement_entered = false;
-	int input_line = 0;
+	std::string line;
+
+	char buff[101], *ptr, *pnum;
+	int cnt = 0;
 
 	std::cout << "=== Advent of Code - day 2 ====" << std::endl;
-
 	std::cout << "--- part 1 ---" << std::endl;
 
-	f = fopen("input.txt", "r");
-	if (f == NULL) {
+	input.open("input.txt", std::ifstream::in);
+
+	if (input.fail()) {
 		std::cout << "Error opening input file.\n";
 		return -1;
 	}
 
-	while (fgets(buff, 101, f) != NULL) {
+	while (std::getline(input, line)) {
 		int dims[3], idx = 0, tmp;
-		
-		input_line++;
-		ptr = buff;
-		while (idx < 3){
-			pnum = ptr;
-			while ((*pnum <= '9') && (*pnum >= '0')){
-				pnum++;
-			}
-			if ((*pnum == 'x') || (*pnum == 'X') || (*pnum == 0) || (*pnum == '\n') || (*pnum == '\r')){
-				*pnum = 0;
-				dims[idx] = atoi(ptr);
-				idx++;
-				ptr = ++pnum;
-			}
-			else{
-				std::cout << "Invalid input format at line " << input_line << std::endl;
-				return -1;
-			} 
+		std::regex regex_dimensions("^(\\d+)x(\\d+)x(\\d+)$");
+		std::smatch sm;
+
+		cnt++;
+
+		if (!regex_match(line, sm, regex_dimensions)) {
+			std::cout << "Invalid input format at line " << cnt << std::endl;
+			return -1;
 		}
+
+		dims[0] = atoi(sm.str(1).c_str());
+		dims[1] = atoi(sm.str(2).c_str());
+		dims[2] = atoi(sm.str(3).c_str());
 
 		if (dims[0] > dims[1]) {
 			tmp = dims[1];
@@ -52,14 +48,16 @@ int main(void) {
 			dims[2] = dims[1];
 			dims[1] = tmp;
 		}
-		result1 += (2 * (dims[0] * dims[1])) + (2 * (dims[0] * dims[2])) + (2 * (dims[1] * dims[2])) + (dims[0]*dims[1]);
-		result2 += (2 * dims[0]) + (2 * dims[1]) + (dims[0] * dims[1] * dims[2]);
+		result1 += (2 * (dims[0] * dims[1])) + (2 * (dims[0] * dims[2])) +
+				   (2 * (dims[1] * dims[2])) + (dims[0] * dims[1]);
+		result2 +=
+			(2 * dims[0]) + (2 * dims[1]) + (dims[0] * dims[1] * dims[2]);
 	}
 
-	if (f != NULL) {
-		fclose(f);
-		f = NULL;
+	if (input.is_open()) {
+		input.close();
 	}
+
 	std::cout << "Result is " << result1 << std::endl;
 	std::cout << "--- part 2 ---" << std::endl;
 	std::cout << "Result is " << result2 << std::endl;
