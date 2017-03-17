1a708bdcd106e24c47482bf6efdc139f055176fc
Petr Faltýnek <pfaltynek@gmail.com>
2017-03-17 14:15:21 +0100

    updated solution of day 4 to use c++ libraries

--- day04/main.cpp
+++ day04/main.cpp
@@ -1,32 +1,31 @@
 #include <cstdio>
 #include <iostream>
-#include <openssl/md5.h>
-#include <string.h>
+#include "md5.h"
 
-const char input[] = "bgvyzdsv";
+
+const std::string input = "bgvyzdsv";
 char buffer[101];
-unsigned char hash[MD5_DIGEST_LENGTH];
 
 int main(void) {
 	int result1 = 0, result2 = 0, number = 0, count;
 	int size;
+	std::string hash;
+	MD5 md5;
 
 	std::cout << "=== Advent of Code - day 4 ====" << std::endl;
 	std::cout << "--- part 1 ---" << std::endl;
 
 	while ((result1 == 0) || (result2 == 0)) {
-		sprintf(buffer, "%s%d", input, number);
-		size = strlen(buffer);
-		MD5((const unsigned char *)buffer, size, hash);
-		count = 0;
-		if (hash[0] == 0 && hash[1] == 0){
-			if (((hash[2] & 0xF0) == 0) && (result1 == 0)) {
-				result1 = number;
-			}
-			if ((hash[2]  == 0) && (result2 == 0)) {
-				result2 = number;
-			}
+		sprintf(buffer, "%s%d", input.c_str(), number);
+		hash = md5.digestString(buffer);
+
+		if ((hash.substr(0,5) == "00000") && (result1 == 0)) {
+			result1 = number;
 		}
+		if ((hash.substr(0,6) == "000000") && (result2 == 0)) {
+			result2 = number;
+		}
+
 		number++;
 	}
 
