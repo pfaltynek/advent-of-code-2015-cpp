00571a1bbb1d08edaf04bd4c28e708b4cd97658d
Petr Faltýnek <pfaltynek@gmail.com>
2017-03-29 11:18:11 +0200

    finished solution of day 13

--- day13/main.cpp
+++ day13/main.cpp
@@ -23,11 +23,44 @@ bool DecodeUnits(std::string line, std::string &who, std::string &next_to, int &
 	return true;
 }
 
+int CalculateHappiness(std::map<std::string, std::map<std::string, int>> units, std::vector<std::string> items) {
+	int result = 0;
+
+	if (items.size() <= 1) {
+		return 0;
+	}
+
+	for (int i = 0; i < items.size() - 1; i++) {
+		result += units[items[i]][items[i + 1]];
+		result += units[items[i + 1]][items[i]];
+	}
+
+	result += units[items[0]][items[items.size() - 1]];
+	result += units[items[items.size() - 1]][items[0]];
+
+	return result;
+}
+
+int GetBestHappiness(std::map<std::string, std::map<std::string, int>> units, std::vector<std::string> items) {
+	int result = 0, tmp;
+
+	std::sort(items.begin(), items.end());
+
+	do {
+		tmp = CalculateHappiness(units, items);
+		if (tmp > result) {
+			result = tmp;
+		}
+	} while (std::next_permutation(items.begin(), items.end()));
+
+	return result;
+}
+
 int main(void) {
 	std::string who, next_to, line;
 	int units, result1, result2, cnt;
 	std::ifstream ifs;
-	std::map<std::string, std::map<std::string, int>> map;
+	std::map<std::string, std::map<std::string, int>> happiness_map;
 	std::vector<std::string> items;
 
 	std::cout << "=== Advent of Code - day 13 ====" << std::endl;
@@ -45,30 +78,30 @@ int main(void) {
 	while (std::getline(ifs, line)) {
 		cnt++;
 		if (DecodeUnits(line, who, next_to, units)) {
-
+			if (std::find(items.begin(), items.end(), who) == items.end()) {
+				items.push_back(who);
+			}
+			if (std::find(items.begin(), items.end(), next_to) == items.end()) {
+				items.push_back(next_to);
+			}
+			happiness_map[who][next_to] = units;
 		} else {
 			std::cout << "Error decoding input line " << cnt << std::endl;
 			return -1;
 		}
 	}
 
-	/* hint:
-  int myints[] = {1,2,3};
-
-  std::sort (myints,myints+3);
-
-  std::cout << "The 3! possible permutations with 3 elements:\n";
-  do {
-    std::cout << myints[0] << ' ' << myints[1] << ' ' << myints[2] << '\n';
-  } while ( std::next_permutation(myints,myints+3) );
-
-  std::cout << "After loop: " << myints[0] << ' ' << myints[1] << ' ' << myints[2] << '\n';
-  */
-
 	if (ifs.is_open()) {
 		ifs.close();
 	}
-	std::cout << "Result is '" << result1 << "'" << std::endl;
+
+	result1 = GetBestHappiness(happiness_map, items);
+
+	std::cout << "Result is " << result1 <<std::endl;
 	std::cout << "--- part 2 ---" << std::endl;
-	std::cout << "Result is '" << result2 << "'" << std::endl;
+
+	items.push_back("Jouza_Jouzovaty");
+	result2 = GetBestHappiness(happiness_map, items);
+
+	std::cout << "Result is " << result2 << std::endl;
 }
