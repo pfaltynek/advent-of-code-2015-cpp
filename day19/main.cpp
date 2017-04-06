311fdd58e2c1e35f0af242fbb0c57b55cb13fbf6
Petr Faltýnek <pfaltynek@gmail.com>
2017-04-06 15:05:26 +0200

    started solution of part 2 for day 19

--- day19/main.cpp
+++ day19/main.cpp
@@ -21,17 +21,62 @@ bool DecodeReplacement(std::string line, std::string &left, std::string &right) 
 	return true;
 }
 
+int SolvePart1(std::string molecule, std::map<std::string, std::vector<std::string>> replacements) {
+	std::map<std::string, int> distinct;
+	size_t last_pos = 0;
+
+	for (std::map<std::string, std::vector<std::string>>::iterator it = replacements.begin(); it != replacements.end(); ++it) {
+		last_pos = molecule.find(it->first, 0);
+		while (last_pos != std::string::npos) {
+			for (int i = 0; i < it->second.size(); i++) {
+				std::string tmp = molecule;
+				tmp.replace(last_pos, it->first.size(), it->second[i]);
+
+				distinct[tmp]++;
+			}
+			last_pos = molecule.find(it->first, last_pos + 1);
+		}
+	}
+
+	return distinct.size();
+}
+
+void SolvePart2(std::string &molecule, int steps, std::map<std::string, std::string> &replacements, int &result) {
+	steps++;
+
+	if (molecule == "e") {
+		if (steps < result) {
+			result = steps;
+		}
+		return;
+	} else if (steps >= result) {
+		return;
+	}
+	for (std::map<std::string, std::string>::iterator it = replacements.begin(); it != replacements.end(); ++it) {
+		size_t pos = 0;
+		pos = molecule.find(it->first, pos);
+		while (pos != std::string::npos) {
+			molecule.replace(pos, it->first.size(), it->second);
+			SolvePart2(molecule, steps, replacements, result);
+			molecule.replace(pos, it->second.size(), it->first);
+			pos = molecule.find(it->first, pos + 1);
+		}
+	}
+}
+
 int main(void) {
 	int result1, result2, cnt;
 	std::ifstream ifs;
 	std::string line, left, right, molecule;
 	std::map<std::string, std::vector<std::string>> map;
+	std::map<std::string, std::string> rev_map;
 	bool replacements_decoded = false;
 
 	std::cout << "=== Advent of Code - day 19 ====" << std::endl;
 	std::cout << "--- part 1 ---" << std::endl;
 
-	result2 = result1 = cnt = 0;
+	result1 = cnt = 0;
+	result2 = INT_MAX;
 
 	ifs.open("input.txt", std::ifstream::in);
 
@@ -61,6 +106,7 @@ int main(void) {
 				new1.push_back(right);
 				map[left] = new1;
 			}
+			rev_map[right] = left;
 		} else {
 			std::cout << "Error decoding input line " << cnt << std::endl;
 			return -1;
@@ -71,7 +117,12 @@ int main(void) {
 		ifs.close();
 	}
 
+	result1 = SolvePart1(molecule, map);
+
 	std::cout << "Result is " << result1 << std::endl;
 	std::cout << "--- part 2 ---" << std::endl;
+
+	SolvePart2(molecule, 0, rev_map, result2);
+
 	std::cout << "Result is " << result2 << std::endl;
 }
